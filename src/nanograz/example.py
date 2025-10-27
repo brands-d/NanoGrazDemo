063095a046f725a6cb98d111e7fc5706627fc3c2
Dominik Brandstetter <dominik.brandstetter@edu.uni-graz.at>
2025-10-27 11:47:12 +0100

    Type annotations demo.

--- src/nanograz/example.py
+++ src/nanograz/example.py
@@ -9,3 +9,19 @@ def square(x: int | float) -> int | float:
         int|float: The square of the input number.
     """
     return x * x
+
+
+def foo(x: int, y: str = "foo"):
+    """
+    Demo function to demonstrate type annotations.
+
+    Args:
+        x (int): An integer input.
+        y (str, optional): A string input. Defaults to "foo".
+
+    Returns:
+        int
+    """
+
+    z: int = x * len(y)
+    return z
--- tests/test_example.py
+++ tests/test_example.py
@@ -15,3 +15,8 @@ def output():
 def test_square(input, output):
     for i, o in zip(input, output):
         assert example.square(i) == o
+
+
+def test_foo():
+    assert example.foo(3) == 9
+    assert example.foo(2, "example") == 14
